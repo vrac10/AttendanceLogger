3088bfb4d6bd7b0e6f5f431673b0d4592964cbfa
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-10-06 22:42:16 +0530

    Reformat file

--- main.go
+++ main.go
@@ -1,31 +1,31 @@
 package main
 
 import (
-  "fmt"
-  "bufio"
-  "log"
-  "os"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
-  fmt.Println("Enter the student name:")
+	fmt.Println("Enter the student name:")
 
-  inputReader := bufio.NewReader(os.Stdin)
-  name, _ := inputReader.ReadString('\n')
+	inputReader := bufio.NewReader(os.Stdin)
+	name, _ := inputReader.ReadString('\n')
 
-  file, err := os.Create("attendance.txt")
-  if err != nil {
-    //TODO Add error message here
-    log.Fatalf("%s", err)
-  }
+	file, err := os.Create("attendance.txt")
+	if err != nil {
+		//TODO Add error message here
+		log.Fatalf("%s", err)
+	}
 
-  defer file.Close()
+	defer file.Close()
 
-  _ , err2 := file.WriteString(name)
+	_, err2 := file.WriteString(name)
 
-  if err2 != nil {
-    //TODO Add error message here
-    log.Fatalf("%s", err)
-  }
+	if err2 != nil {
+		//TODO Add error message here
+		log.Fatalf("%s", err)
+	}
 
 }
