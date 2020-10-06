02da3f5605f41bc89b7c345515576b8a91508492
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-10-06 22:29:40 +0530

    Init, Adding a name works

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+  "fmt"
+  "bufio"
+  "log"
+  "os"
+)
+
+func main() {
+  fmt.Println("Enter the student name:")
+
+  inputReader := bufio.NewReader(os.Stdin)
+  name, _ := inputReader.ReadString('\n')
+
+  file, err := os.Create("attendance.txt")
+  if err != nil {
+    //TODO Add error message here
+    log.Fatalf("%s", err)
+  }
+
+  defer file.Close()
+
+  _ , err2 := file.WriteString(name)
+
+  if err2 != nil {
+    //TODO Add error message here
+    log.Fatalf("%s", err)
+  }
+
+}
