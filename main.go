e4d848ff093ed6ca7323135d40ea029930386881
vrac10 <vrachit106@gmail.com>
2023-04-28 19:43:38 +0530

    Added the feature to create logs based on specific classes

--- main.go
+++ main.go
@@ -11,22 +11,25 @@ import (
 	"time"
 )
 
-//Funtion for logging errors
+// Funtion for logging errors
 func errorHandler(err error) {
 	println("Ops, something went wrong:", err)
 }
 
 //Function to get attendance
 
-
-
 func viewAttendance() {
 	println("This will show you attendance")
-	
-	if _, err := os.Stat("attendance.txt"); os.IsNotExist(err) {
+
+	fmt.Println("Which class's attendance would you like to view?:")
+
+	Reader := bufio.NewReader(os.Stdin)
+	classname1, _ := Reader.ReadString('\n')
+
+	if _, err := os.Stat(classname1); os.IsNotExist(err) {
 		println("No attendance to show")
-	} else{
-		file, err := os.Open("attendance.txt")
+	} else {
+		file, err := os.Open(classname1)
 		if err != nil {
 			errorHandler(err)
 			log.Fatal(err)
@@ -45,36 +48,45 @@ func viewAttendance() {
 
 	}
 
+}
 
-}	
-
-func resetAttendance(){
+func resetAttendance() {
 	println("This will reset attendance")
-	
-	if _, err := os.Stat("attendance.txt"); os.IsNotExist(err) {
+
+	fmt.Println("Which class's attendance do you wish to reset:")
+
+	inputReader := bufio.NewReader(os.Stdin)
+	classname, _ := inputReader.ReadString('\n')
+
+	if _, err := os.Stat(classname); os.IsNotExist(err) {
 		println("Attendance already clear")
-	} else{
-		e := os.Remove("attendance.txt")
+	} else {
+		e := os.Remove(classname)
 		if e != nil {
 			errorHandler(e)
 			log.Fatal(e)
-		} else{
+		} else {
 			println("Attendance Cleared")
 		}
 	}
 }
 
-func getStudentInfo() (normtime, epochtime, name, roll, course string) {
-	now:= time.Now()
+func getStudentInfo() (normtime, epochtime, name, class, roll, course string) {
+	now := time.Now()
 	fmt.Println("Time: ", now.Local(), "\n")
-	epoch:=now.Unix()
-	norm:=now.Local()
-	epochtime=fmt.Sprint(epoch)
-	normtime=fmt.Sprint(norm)
+	epoch := now.Unix()
+	norm := now.Local()
+	epochtime = fmt.Sprint(epoch)
+	normtime = fmt.Sprint(norm)
 	fmt.Println("Enter the student name:")
 
 	inputReader := bufio.NewReader(os.Stdin)
 	name, _ = inputReader.ReadString('\n')
+
+	fmt.Println("Enter the Student class/section:")
+
+	inputReader = bufio.NewReader(os.Stdin)
+	class, _ = inputReader.ReadString('\n')
 
 	fmt.Println("Enter the student roll number:")
 
@@ -86,51 +98,50 @@ func getStudentInfo() (normtime, epochtime, name, roll, course string) {
 	inputReader = bufio.NewReader(os.Stdin)
 	course, _ = inputReader.ReadString('\n')
 
-	return strings.TrimSpace(normtime), strings.TrimSpace(epochtime), strings.TrimSpace(name), strings.TrimSpace(roll), strings.TrimSpace(course)
+	return strings.TrimSpace(normtime), strings.TrimSpace(epochtime), strings.TrimSpace(name), strings.TrimSpace(class), strings.TrimSpace(roll), strings.TrimSpace(course)
 
 }
 
-//Main
+// Main
 func main() {
-	
+
 	fmt.Println("Type \n")
 	fmt.Println("1 to view attendance")
 	fmt.Println("2 to log attendance")
 	fmt.Println("3 to reset attendance")
-	
+
 	var option int
-	
+
 	fmt.Scanln(&option)
-	
+
 	switch option {
-		case 1:
-			viewAttendance()
-		case 2:
-			normtime, epochtime, name, roll, course := getStudentInfo()
-			record := []string{normtime, epochtime, name, roll, course}
+	case 1:
+		viewAttendance()
+	case 2:
+		normtime, epochtime, name, class, roll, course := getStudentInfo()
+		record := []string{normtime, epochtime, name, class, roll, course}
 
-			file, err := os.OpenFile("attendance.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
-			if err != nil {
-				panic(err)
-			}
-	
+		filename := class + "attendance.txt"
 
-			w := csv.NewWriter(file)
+		file, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
+		if err != nil {
+			panic(err)
+		}
 
-			defer file.Close()
+		w := csv.NewWriter(file)
 
-			w.Write(record)
-			w.Flush()
-			err = w.Error()
+		defer file.Close()
 
-			if err != nil {
-				errorHandler(err)
-				log.Fatalf("%s", err)
-			}
-		case 3:
-			resetAttendance()
+		w.Write(record)
+		w.Flush()
+		err = w.Error()
+
+		if err != nil {
+			errorHandler(err)
+			log.Fatalf("%s", err)
+		}
+	case 3:
+		resetAttendance()
 	}
-	
-	
-	
+
 }
